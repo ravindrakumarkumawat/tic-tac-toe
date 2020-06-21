88367f6fb10675edf3cbcc179d61872fabb8eb76
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-06-21 22:40:12 +0530

    State is manage by boards now

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      value: null
-    }
-  }
-
   render () {
     return (
       <button
         className='square'
-        onClick={() => this.setState({ value: 'X' })}
+        onClick={() => this.props.onClick()}
       >
         {/* TODO */}
-        {this.state.value}
+        {this.props.value}
       </button>
     )
   }
@@ -27,12 +20,23 @@ class Board extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      squares: Array(9).fill()
+      squares: Array(9).fill(null)
     }
   }
 
+  handleClick (i) {
+    const squares = this.state.squares.slice()
+    squares[i] = 'X'
+    this.setState({ squares: squares })
+  }
+
   renderSquare (i) {
-    return <Square value={this.state.squares[i]} />
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    )
   }
 
   render () {
