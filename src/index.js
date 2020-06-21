8c792105dda05c2490ce9f59255bd850c1f271cb
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-06-21 19:59:35 +0530

    Created a interactive component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 class Square extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      value: null
+    }
+  }
+
   render () {
     return (
-      <button className='square'>
+      <button
+        className='square'
+        onClick={() => this.setState({ value: 'X' })}
+      >
         {/* TODO */}
-        {this.props.value}
+        {this.state.value}
       </button>
     )
   }
